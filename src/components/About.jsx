9ef4ab1b563b38bab5b709e9271fc5a1a516ec79
Nova <nova@Novas-MacBook-Pro.local>
2025-01-05 13:57:33 +0700

    first Project

---
+++ src/components/About.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+import { SectionWrapper } from '../hoc';
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <motion.div
+      variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
+      className="xs:w-[250px] w-full card-gradient p-[1px] rounded-[20px] shadow-card">
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-jetLight rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+        <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+        <h3 className="text-taupe text-[18px] font-bold text-center">
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  );
+};
+
+const About = () => {
+  return (
+    <div className="-mt-[6rem]">
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn('', '', 0.1, 1)}
+        className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]">
+        I am a passionate and detail-oriented web developer with experience in designing and building dynamic, responsive websites and web applications. With proficiency in front-end technologies like HTML, CSS, and JavaScript, as well as back-end technologies such as Node.js and databases like MySQL, I create seamless, user-centric experiences. I am constantly learning and adapting to new tools, frameworks, and best practices to ensure high-quality, scalable solutions. My goal is to help businesses and individuals create impactful, functional websites that not only look great but perform flawlessly across all devices.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SectionWrapper(About, 'about');
---
+++ src/constants/index.js
@@ -0,0 +1,223 @@
+import {
+  frontend,
+  backend,
+  ux,
+  prototyping,
+  javascript,
+  typescript,
+  job_service,
+  cms,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  git,
+  figma,
+  docker,
+  postgresql,
+  rubyrails,
+  graphql,
+  komikult,
+  e_commerce,
+  pos,
+  leaderboard,
+  math,
+  movie,
+  nyeusi,
+  space,
+  coverhunt,
+  dcc,
+  kelhel,
+  microverse,
+} from '../assets';
+
+export const navLinks = [
+  {
+    id: 'about',
+    title: 'About',
+  },
+  {
+    id: 'projects',
+    title: 'Projects',
+  },
+  // {
+  //   id: 'contact',
+  //   title: 'Contact',
+  // },
+];
+
+const services = [
+  {
+    title: 'Frontend Developer',
+    icon: frontend,
+  },
+  {
+    title: 'Backend Developer',
+    icon: backend,
+  },
+  {
+    title: 'UI/UX Design',
+    icon: ux,
+  },
+];
+
+const technologies = [
+  {
+    name: 'HTML 5',
+    icon: html,
+  },
+  {
+    name: 'CSS 3',
+    icon: css,
+  },
+  {
+    name: 'JavaScript',
+    icon: javascript,
+  },
+  {
+    name: 'TypeScript',
+    icon: typescript,
+  },
+  {
+    name: 'React JS',
+    icon: reactjs,
+  },
+  {
+    name: 'Redux Toolkit',
+    icon: redux,
+  },
+  {
+    name: 'Tailwind CSS',
+    icon: tailwind,
+  },
+  {
+    name: 'Node JS',
+    icon: nodejs,
+  },
+  {
+    name: 'Rails',
+    icon: rubyrails,
+  },
+  {
+    name: 'graphql',
+    icon: graphql,
+  },
+  {
+    name: 'postgresql',
+    icon: postgresql,
+  },
+  {
+    name: 'git',
+    icon: git,
+  },
+  {
+    name: 'figma',
+    icon: figma,
+  },
+  {
+    name: 'docker',
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: 'Front-End Developer',
+    company_name: 'Codingate',
+    icon: coverhunt,
+    iconBg: '#333333',
+    date: 'Jan 2022 - Dec 2022',
+  },
+  {
+    title: 'Web Developer',
+    company_name: 'CAIT',
+    icon: microverse,
+    iconBg: '#333333',
+    date: 'Jan 2024 - Present',
+  }
+];
+
+const projects = [
+  {
+    id: 'project-1',
+    name: 'E-commerce',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'mongodb',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'tailwind',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: e_commerce,
+  },
+  {
+    id: 'project-3',
+    name: 'Job Service',
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: job_service
+  },
+  {
+    id: 'project-4',
+    name: 'Management System',
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: cms,
+  },
+  {
+    id: 'project-5',
+    name: 'POS',
+    description:
+      'This is a demo concert website for a music festival called Nyeusi.',
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: pos,
+  },
+];
+
+export { services, technologies, experiences, projects };
